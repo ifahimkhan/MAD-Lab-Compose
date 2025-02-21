5e2861c93fd3c86bf4df3b599f80a12070313f7f
ifahimkhan <khan.fahim95@hotmail.com>
2025-02-22 00:10:40 +0530

    if applied this commit will add File operation in compose

---
+++ app/src/main/java/com/fahim/mad_lab_compose/model/InternalStorage.kt
@@ -0,0 +1,37 @@
+package com.fahim.mad_lab_compose.model
+
+import android.content.Context
+import android.widget.Toast
+import com.fahim.mad_lab_compose.interfaces.Storage
+import java.io.IOException
+
+class InternalStorage : Storage {
+    override fun writeToFile(data: String, context: Context, fileName: String) {
+        try {
+            context.openFileOutput(fileName, Context.MODE_PRIVATE).use { fos ->
+                fos.write(data.toByteArray())
+                Toast.makeText(context, "File Written Successfully", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun readFromFile(context: Context,fileName: String): String {
+
+        val stringBuilder = StringBuilder()
+
+        try {
+            context.openFileInput(fileName).use { fis ->
+                var content: Int
+                while ((fis.read().also { content = it }) != -1) {
+                    stringBuilder.append(content.toChar())
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return stringBuilder.toString()
+    }
+}
