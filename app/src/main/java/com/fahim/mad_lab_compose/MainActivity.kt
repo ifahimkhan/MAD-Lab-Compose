3e44bebcd6e424d1518430bf6663a927a4923ce6
ifahimkhan <khan.fahim95@hotmail.com>
2025-03-04 20:55:14 +0530

    added GradientCard in compose

--- app/src/main/java/com/fahim/mad_lab_compose/MainActivity.kt
+++ app/src/main/java/com/fahim/mad_lab_compose/MainActivity.kt
@@ -3,27 +3,38 @@ package com.fahim.mad_lab_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fahim.mad_lab_compose.ui.theme.MADLabComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MADLabComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        GradientCard()
+                    }
                 }
             }
         }
@@ -31,17 +42,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GradientCard() {
+    val gradientBrush = Brush.verticalGradient(
+        colors = listOf(
+            Color(0xFF6A1B9A), // Purple
+            Color(0xFFAB47BC)  // Light Purple
+        )
     )
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .height(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradientBrush)
+        ) {
+            Text(
+                text = "Gradient Card",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp),
+                color = Color.White
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    MADLabComposeTheme {
-        Greeting("Android")
+fun PreviewGradientCard() {
+    MaterialTheme {
+        GradientCard()
     }
 }
